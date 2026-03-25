a3b48a29b7e5d07d1a3ebf0075972f4742d85f2e
ponyatova <ponyatova.95@mail.ru>
2026-03-25 21:16:21 +0400

    Update page.tsx

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,7 +1,7 @@
 import PrivacyPageClient from "./PrivacyPageClient";
 
 export default async function PrivacyPage() {
-  const url = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const url = "https://dr-aponyatova.vercel.app";
   const email = process.env.NEXT_PUBLIC_CONTACT_EMAIL || "";
   return <PrivacyPageClient url={url} email={email} />;
 }
