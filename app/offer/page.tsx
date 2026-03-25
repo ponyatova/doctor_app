f6bd5ed35d82a0c94c669dfd772ad97236ad789a
ponyatova <ponyatova.95@mail.ru>
2026-03-25 21:15:51 +0400

    Update page.tsx

--- app/offer/page.tsx
+++ app/offer/page.tsx
@@ -2,7 +2,7 @@ import ClientPageOffer from "./ClientPageOffer";
 
 export default async function OfferPage() {
  
-  const url = https://dr-aponyatova.vercel.app
+  const url = "https://dr-aponyatova.vercel.app"
   const email = process.env.NEXT_PUBLIC_CONTACT_EMAIL || "";
   return <ClientPageOffer url={url} email={email} />;
 }
